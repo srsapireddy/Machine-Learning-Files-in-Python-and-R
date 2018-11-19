a108e3dac6337b3b86d66c245f5801479bfe5793
srsapireddy <32967087+srsapireddy@users.noreply.github.com>
2018-11-18 18:09:03 -0600

    Add files via upload

---
+++ Dimensionality Reduction/Principal Component Analysis/pca.R
@@ -0,0 +1,79 @@
+# PCA
+# Extract a smaller number of independent variables that are going to be new independent variables like new dimentions. These new independent variables are going to be some new independent variables that explains the most of variance of the dataset.
+# This is an unsupervised model as we dont consider the dependent variable in the model.
+# Unsupervised --> Meaning we dont need the dependent variable to extract the features.
+# Extracted features of the variables --> Principle Components.
+
+# Importing the dataset
+dataset = read.csv('Wine.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Customer_Segment, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-14] = scale(training_set[-14])
+test_set[-14] = scale(test_set[-14])
+
+# Applying PCA
+install.packages('caret')
+library(caret)
+install.packages('e1071')
+library(e1071)
+pca = preProcess(x = training_set[-14], method = 'pca', pcaComp = 2)
+training_set = predict(pca, training_set)
+training_set = training_set[c(2, 3, 1)]
+test_set = predict(pca, test_set)
+test_set = test_set[c(2, 3, 1)]
+# Parameters
+# [1] x --> Dataset
+# [2] method  --> pca
+# [3] thresh --> If we want the dimensionality of the dataset with atleast a minimum amount of explained variance.
+# [4] pcaComp --> Specific number of pca components to keep. That is number of extracted features we want to extract at the end that will explain the most variance.
+# This over-rides thresh parameter.
+
+# Fitting SVM to the Training set
+# install.packages('e1071')
+library(e1071)
+classifier = svm(formula = Customer_Segment ~ ., data = training_set, type = 'C-classification', kernel = 'linear')
+
+# Predicting the Test Set Results
+y_pred = predict(classifier, newdata = test_set)
+
+# Making a Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('PC1', 'PC2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'SVM (Training set)',
+     xlab = 'PC1', ylab = 'PC2',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('PC1', 'PC2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3], main = 'SVM (Test set)',
+     xlab = 'PC1', ylab = 'PC2',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
